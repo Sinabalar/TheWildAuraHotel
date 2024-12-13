6255f60b5144f83f31b923c68e6327c54c1f24c7
Sinabalar <sinabalar666@gmail.com>
2024-12-14 00:40:28 +0330

    index.css file are removed and all the styles moved to globalStyles.js & all of styledComponents moved to it's file & fontFamily added to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import GlobalStyles from "./styles/globalStyles.js";
+import Button from ".././src/ui/Button.jsx";
+import Heading from "./ui/Heading.jsx";
+import Input from "./ui/Input.jsx";
+import StyledApp from "./ui/StyledApp.jsx";
 
 export default function App() {
     return (
-        <div>
-            test
-        </div>
+        <>
+            <GlobalStyles/>
+            <StyledApp>
+                <Heading>The Wild Aura</Heading>
+                <Button onClick={() => alert('checked in')}>Check in</Button>
+                <Button onClick={() => alert('checked out')}>Check out</Button>
+                <Input type={"number"} placeholder={"number of guests"}/>
+            </StyledApp>
+        </>
     );
 }
---
+++ src/ui/StyledApp.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+    background-color: orangered;
+    padding: 20px;
+`;
+
+export default StyledApp
