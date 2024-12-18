f62f960d6daeeac7a0756329c158be8e77c94a78
Sinabalar <sinabalar666@gmail.com>
2024-12-18 22:33:30 +0330

    feat: add toggleable form for creating new cabins
    - Import `useState` hook for managing form visibility state in `Cabins.jsx`
    - Add `Button` component to toggle form visibility
    - Add `CreateCabinForm` component to render form conditionally based on state

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import {useState} from "react";
+import Button from "../ui/Button.jsx";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 
 function Cabins() {
+
+    const [showForm, setShowForm] = useState(false)
 
     return (
         <>
@@ -12,6 +17,10 @@ function Cabins() {
             </Row>
             <Row>
                 <CabinTable/>
+                <Button onClick={() => setShowForm(curr => !curr)}>{showForm?"Close form":"Add new cabin"}</Button>
+
+                {showForm && <CreateCabinForm/>}
+
             </Row>
         </>
     );
