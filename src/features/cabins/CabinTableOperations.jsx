0208f6c60b0f57d171f4e3a96e33c105e417059b
Sinabalar <sinabalar666@gmail.com>
2025-01-19 21:18:04 +0330

    feat: Add discount filtering to cabin table
    - Implement discount filter UI with filter buttons
    - Add filter logic in CabinTable using searchParams
    - Replace static filter text with CabinTableOperations component
    - Remove styled TableHeader in favor of Table component

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,10 @@
+import TableOperations from "../../ui/TableOperations.jsx";
+import Filter from "../../ui/Filter.jsx";
+
+export default function CabinTableOperations() {
+    return (
+        <TableOperations>
+            <Filter/>
+        </TableOperations>
+    );
+}
